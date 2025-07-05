497af845eae3b6454462e0741af36423debfdb24
ardiyansyah323 <243320030157ardiyansyah@apps.ipb.ac.id>
2025-07-05 20:43:11 +0700

    Add files via upload

---
+++ kirim ke imam/data.js
@@ -0,0 +1,18 @@
+const nama = "ardi";
+let usia = 19;
+
+
+function generatebiodata() {
+    if(usia == 30) {
+     // kondisi pertama
+     console.log('anda dewasa')
+    } else {
+     // kondisi kedua 
+     console.log('anda anak anak')
+    }
+     }
+
+console.log(nama);
+console.log(usia);
+
+generatebiodata();
